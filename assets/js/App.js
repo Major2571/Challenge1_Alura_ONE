c060b6281cd201686d8220aa854f02e52f9e2a9f
Major2571 <caroline.tacats62@gmail.com>
2024-01-31 23:17:16 -0300

    add encode functions

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,6 +1,8 @@
 function clearText() {
   document.getElementById("inputText").value = "";
   document.getElementById("outputText").value = "";
+  showEmptyMessage();
+  verifyEncode();
 }
 
 function speakText(source) {
@@ -21,6 +23,49 @@ function copyToClipboard() {
   navigator.clipboard.writeText(text);
 }
 
+function removeAccents(text) {
+  return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let inputText = document.getElementById("inputText");
+  let btnEncodeDecode = document.getElementById("btn-encode");
+  btnEncodeDecode.innerText = "Codificar";
+
+  inputText.addEventListener("input", function () {
+    let text = inputText.value.toLowerCase();
+    let isEncoded = isEncodedText(text);
+
+    if (isEncoded) {
+      btnEncodeDecode.innerText = "Decodificar";
+    }
+  });
+});
+
+function verifyEncode() {
+  let inputText = document.getElementById("inputText").value.trim();
+  let outputText = document.getElementById("outputText").value.trim();
+  let btnClear = document.querySelector(".btn-clear");
+  let btnSpeak = document.getElementById("btnSpeak");
+  let btnSpeakOutput = document.getElementById("btnSpeakOutput");
+
+  if (inputText !== "") {
+    btnClear.classList.remove("btn-hidden");
+    btnSpeak.classList.remove("btn-hidden");
+  } else {
+    btnClear.classList.add("btn-hidden");
+    btnSpeak.classList.add("btn-hidden");
+  }
+
+  if (outputText !== "") {
+    btnSpeakOutput.classList.remove("btn-hidden");
+  } else {
+    btnSpeakOutput.classList.add("btn-hidden");
+  }
+}
+
+  
+
 function showEmptyMessage() {
   let outputTextArea = document.getElementById("outputText");
   let outputEmptyMessage = document.getElementById("outputEmptyMessage");
@@ -30,4 +75,68 @@ function showEmptyMessage() {
   } else {
     outputEmptyMessage.style.display = "none";
   }
+}
+
+function processText() {
+  let inputText = document.getElementById("inputText").value;
+
+  let isEncoded = isEncodedText(inputText);
+
+  if (isEncoded) {
+    let decodedTextResult = decodedText(inputText);
+    document.getElementById("outputText").value = decodedTextResult;
+  } else {
+    let inputTextWithoutAccents = removeAccents(inputText.toLowerCase());
+    let encodedTextResult = encodeText(inputTextWithoutAccents);
+    document.getElementById("outputText").value = encodedTextResult;
+  }
+
+  verifyEncode();
+  showEmptyMessage();
+}
+
+document.getElementById("inputText").addEventListener("input", processText);
+
+function encodeText(text) {
+  const VOWEL_REPLACEMENT = {
+    a: "ai",
+    e: "enter",
+    i: "imes",
+    o: "ober",
+    u: "ufat",
+  };
+
+  let encodedText = "";
+
+  for (let i = 0; i < text.length; i++) {
+    let currentChar = text[i];
+    let replacement = VOWEL_REPLACEMENT[currentChar];
+    encodedText += replacement !== undefined ? replacement : currentChar;
+  }
+
+  return encodedText;
+}
+
+function isEncodedText(text) {
+  const ENCODED_CODE = ["ai", "enter", "imes", "ober", "ufat"];
+  return ENCODED_CODE.some((code) => text.includes(code));
+}
+
+function decodedText(text) {
+  const DECODE_VOWEL = {
+    ai: "a",
+    enter: "e",
+    imes: "i",
+    ober: "o",
+    ufat: "u",
+  };
+
+  let decodedText = text;
+
+  for (const code in DECODE_VOWEL) {
+    const regex = new RegExp(code, "g");
+    decodedText = decodedText.replace(regex, DECODE_VOWEL[code]);
+  }
+
+  return decodedText;
 }
